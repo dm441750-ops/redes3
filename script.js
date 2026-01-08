d91b70b3c2495d2d630bdb1d3465bd9971f8e481
dm441750-ops <dm441750@gmail.com>
2026-01-08 05:25:29 -0600

    Clean up Firebase initialization code in script.js
    Removed commented-out Firebase initialization code and SDK import comments.

--- script.js
+++ script.js
@@ -22,14 +22,9 @@ from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 const db = getFirestore(app); */
 
 <script type="module">
-  // Import the functions you need from the SDKs you need
    import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
    import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: "AIzaSyAnB_sfpz4JbO-BFrblqpAVMpY4BSAnFA0",
     authDomain: "web-app-df58b.firebaseapp.com",
