8e4125fc8da1a07a58fb549de79e48ad87b2f6be
dm441750-ops <dm441750@gmail.com>
2026-01-08 05:20:28 -0600

    Refactor Firebase setup and enable analytics
    Updated Firebase initialization and configuration to use the latest SDK and added analytics support.

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 /***********************
  * IMPORTS FIREBASE
  ***********************/
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+/*import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
 import { getFirestore, collection, addDoc, serverTimestamp, onSnapshot, query, orderBy } 
 from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 
 /***********************
  * CONFIG FIREBASE
  ***********************/
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: "TU_API_KEY",
   authDomain: "TU_PROYECTO.firebaseapp.com",
   projectId: "TU_PROYECTO",
@@ -18,8 +18,32 @@ const firebaseConfig = {
 /***********************
  * INIT
  ***********************/
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+/*const app = initializeApp(firebaseConfig);
+const db = getFirestore(app); */
+
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAnB_sfpz4JbO-BFrblqpAVMpY4BSAnFA0",
+    authDomain: "web-app-df58b.firebaseapp.com",
+    projectId: "web-app-df58b",
+    storageBucket: "web-app-df58b.firebasestorage.app",
+    messagingSenderId: "1035576649254",
+    appId: "1:1035576649254:web:568f7970c53a409f7ea8e7",
+    measurementId: "G-D3JMYZ1EJN"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
 
 /***********************
  * USUARIO ACTUAL
